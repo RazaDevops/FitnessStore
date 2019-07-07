5e6336baf2879381f1744fc585d9aa3cd5323961
Raza Goilkar <39884974+RazaDevops@users.noreply.github.com>
2019-07-08 01:27:26 +0530

    Updated 7-Jul-19
    New Reg, and login form. Issue: tables not getting created.

--- ECommerce/FitnessStore/models.py
+++ ECommerce/FitnessStore/models.py
@@ -13,15 +13,13 @@ class Person(models.Model):
 
 class custinfo(models.Model):
     cfname = models.CharField(max_length = 50)
-    clname = models.CharField(max_length = 50)
     cmail = models.CharField(max_length = 50)
     cpass = models.CharField(max_length = 50)
-    cdob = models.CharField(max_length = 50)
     ccontact = models.CharField(max_length = 50)
-    cgender = models.CharField(max_length = 50)
 
     class Meta:
         db_table = "custinfo"
+
 
 class strengthprod(models.Model):
     stprodtype = models.CharField(max_length = 50)
@@ -33,5 +31,24 @@ class strengthprod(models.Model):
 
     class Meta:
         db_table = "strengthprod"
+
+'''class yandmprod(models.Model):
+    ynmprodtype = models.CharField(max_length = 50)
+    ynmprodwight = models.CharField(max_length = 50)
+    ynmprodprice = models.FloatField(default=0.0)
+    ynmprodpic = models.ImageField(upload_to = 'static/Images/', default = 'upload pic')
+    def __str__(self):
+        return self.ynmprodtype
+
+    class Meta:
+        db_table = "YandMProd"'''
+
+
+class Cart(models.Model):    
+    prod = models.ForeignKey(strengthprod, on_delete=models.CASCADE)
+    user = models.ForeignKey(custinfo, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "Cart"
 
 
